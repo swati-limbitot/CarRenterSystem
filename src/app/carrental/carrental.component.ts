820314db2fd1be9449452a72dcbd3fe6ec583937
swati-limbitot <swatilimbitot222@gmail.com>
2023-07-04 15:35:49 +0530

    Inital

---
+++ src/app/carrental/carrental.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrental',
+  templateUrl: './carrental.component.html',
+  styleUrls: ['./carrental.component.css']
+})
+export class CarrentalComponent {
+
+}
